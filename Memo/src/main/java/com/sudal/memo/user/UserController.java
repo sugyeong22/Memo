140bb5faa2077599351969163886bfeb09884820
OSG <sugyeong9621@gmail.com>
2025-09-23 23:30:56 +0900

    [오수경] - 회원 가입 입력화면 레이아웃 구성
    #1

---
+++ Memo/src/main/java/com/sudal/memo/user/UserController.java
@@ -0,0 +1,15 @@
+package com.sudal.memo.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+    @GetMapping("/join")
+    public String joinForm(){
+        return "user/join";
+    }
+}
